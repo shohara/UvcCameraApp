02e90586f297d122d8bc193203f6776d62195151
Makoto Shohara <11992036+shohara@users.noreply.github.com>
2024-07-18 19:19:02 +0900

    プロジェクト ファイルを追加します。

---
+++ UvcCameraApp/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace UvcCameraApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelVideo = new System.Windows.Forms.Panel();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.comboBoxCameras = new System.Windows.Forms.ComboBox();
+            this.panelVideo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelVideo
+            // 
+            this.panelVideo.Controls.Add(this.buttonStart);
+            this.panelVideo.Controls.Add(this.comboBoxCameras);
+            this.panelVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelVideo.Location = new System.Drawing.Point(0, 0);
+            this.panelVideo.Name = "panelVideo";
+            this.panelVideo.Size = new System.Drawing.Size(800, 450);
+            this.panelVideo.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(696, 37);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(92, 37);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // comboBoxCameras
+            // 
+            this.comboBoxCameras.FormattingEnabled = true;
+            this.comboBoxCameras.Location = new System.Drawing.Point(570, 4);
+            this.comboBoxCameras.Name = "comboBoxCameras";
+            this.comboBoxCameras.Size = new System.Drawing.Size(227, 26);
+            this.comboBoxCameras.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelVideo);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.panelVideo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelVideo;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ComboBox comboBoxCameras;
+    }
+}
+
---
+++ UvcCameraApp/MainForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using DirectShowLib;
+
+namespace UvcCameraApp
+{
+    public partial class MainForm : Form
+    {
+        private IGraphBuilder graphBuilder;
+        private ICaptureGraphBuilder2 captureGraphBuilder;
+        private IMediaControl mediaControl;
+        private IVideoWindow videoWindow;
+        private IBaseFilter videoRenderer;
+        private DsDevice[] systemCameras;
+        private IBaseFilter sourceFilter; // フィールドとして宣言
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadCameras();
+        }
+
+        private void LoadCameras()
+        {
+            // Get the list of available video input devices (cameras)
+            systemCameras = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+            foreach (var camera in systemCameras)
+            {
+                comboBoxCameras.Items.Add(camera.Name);
+            }
+            if (comboBoxCameras.Items.Count > 0)
+            {
+                comboBoxCameras.SelectedIndex = 0; // Select the first camera by default
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            InitializeDirectShow(comboBoxCameras.SelectedIndex);
+        }
+
+        private void InitializeDirectShow(int cameraIndex)
+        {
+            try
+            {
+                int hr = 0;
+                // Create the Filter Graph Manager
+                graphBuilder = (IGraphBuilder)new FilterGraph();
+
+                // Create the Capture Graph Builder
+                captureGraphBuilder = (ICaptureGraphBuilder2)new CaptureGraphBuilder2();
+                captureGraphBuilder.SetFiltergraph(graphBuilder);
+
+                // Check if there are any cameras
+                if (systemCameras == null || systemCameras.Length == 0)
+                {
+                    MessageBox.Show("No UVC cameras found.");
+                    return;
+                }
+
+                // Bind Moniker to a filter object
+                object source;
+                Guid baseFilterGuid = typeof(IBaseFilter).GUID;
+                systemCameras[cameraIndex].Mon.BindToObject(null, null, ref baseFilterGuid, out source);
+                sourceFilter = (IBaseFilter)source;
+
+                // Add the video capture filter to the graph
+                hr = graphBuilder.AddFilter(sourceFilter, "Video Capture");
+                DsError.ThrowExceptionForHR(hr);
+
+                // Automatically select and add the video renderer
+                hr = captureGraphBuilder.RenderStream(PinCategory.Capture, MediaType.Video, sourceFilter, null, null);
+                DsError.ThrowExceptionForHR(hr);
+
+                // Get the Media Control interface
+                mediaControl = (IMediaControl)graphBuilder;
+
+                // Get the Video Window interface
+                videoWindow = (IVideoWindow)graphBuilder;
+
+                ConfigureVideoWindow();
+
+                // Run the graph
+                hr = mediaControl.Run();
+                DsError.ThrowExceptionForHR(hr);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error initializing DirectShow: " + ex.Message);
+            }
+        }
+
+        private void ConfigureVideoWindow()
+        {
+            try
+            {
+                // Set the video window to the panelVideo control
+                int hr = videoWindow.put_Owner(panelVideo.Handle);
+                DsError.ThrowExceptionForHR(hr);
+
+                hr = videoWindow.put_WindowStyle(WindowStyle.Child | WindowStyle.ClipSiblings);
+                DsError.ThrowExceptionForHR(hr);
+
+                hr = videoWindow.SetWindowPosition(0, 0, panelVideo.Width, panelVideo.Height);
+                DsError.ThrowExceptionForHR(hr);
+
+                hr = videoWindow.put_Visible(OABool.True);
+                DsError.ThrowExceptionForHR(hr);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error configuring video window: " + ex.Message);
+            }
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+
+            // Release DirectShow resources
+            if (mediaControl != null)
+            {
+                mediaControl.Stop();
+            }
+            if (videoRenderer != null)
+            {
+                Marshal.ReleaseComObject(videoRenderer);
+            }
+            if (videoWindow != null)
+            {
+                videoWindow.put_Visible(OABool.False);
+                videoWindow.put_Owner(IntPtr.Zero);
+            }
+            if (graphBuilder != null)
+            {
+                Marshal.ReleaseComObject(graphBuilder);
+            }
+            if (captureGraphBuilder != null)
+            {
+                Marshal.ReleaseComObject(captureGraphBuilder);
+            }
+        }
+    }
+}
